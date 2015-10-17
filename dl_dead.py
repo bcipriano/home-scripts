d211457e6b80f7dd8045c7612f5adf8bcc3c3f84
Brian Cipriano <brian.cipriano@gmail.com>
2015-10-17 13:31:26 -0700

    Rewrote script to use m3u file.

--- dl_dead.py
+++ dl_dead.py
@@ -1,36 +1,85 @@
+#!/usr/bin/env python
+"""
+Downloads Grateful Dead shows from archive.org.
 
-import sys, os
+Provide a link to the URL of the "details" page, for example:
+
+$ ./dl_dead.py https://archive.org/details/gd71-12-10.sbd.yerys.1311.sbeok.shnf
+"""
+
+import argparse
+import os
+import sys
 import urllib2
+from urlparse import urlparse
 
-url = sys.argv[1]
+import requests
 
-response = urllib2.urlopen(url)
-html = response.read()
-html_lines = html.split('\n')
+DL_CHUNK_SIZE = 1024
 
-dl_paths = []
+def main():
+  parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+  parser.add_argument('show_url', help='URL of the show\'s "details" page')
+  args = parser.parse_args()
 
-for html_line in html_lines:
-    if html_line.find('_vbr.mp3') != -1:
-        first_quote = html_line.find('"')
-        second_quote = html_line.find('"', first_quote+1)
-        dl_paths.append(html_line[first_quote+1:second_quote])
+  print 'downloading show from %s' % args.show_url
 
-if url[-1] == '/':
-    url = url[:-1]
+  show_url = urlparse(args.show_url)
+  archive_root = '%s://%s' % (show_url.scheme, show_url.netloc)
+  print 'archive root %s' % archive_root
 
-show_base = url.split('/')[-1]
-show_name = show_base.split('.')[0]
+  # get page source
+  details_src = requests.get(args.show_url).text
 
-target_dir = '%s/Desktop/%s' % (os.getenv('HOME'), show_name)
+  # find m3u file
+  m3u_path = None
+  for src_line in details_src.split('\n'):
+    if '.m3u' in src_line:
+      href = src_line.find('href=')
+      first_quote = src_line.find('"', href)
+      second_quote = src_line.find('"', first_quote+1)
+      m3u_path = src_line[first_quote+1:second_quote]
+      break
 
-print 'Downloading %d files to %s...' % (len(dl_paths), target_dir)
-os.makedirs(target_dir)
+  if m3u_path is None:
+    print 'm3u URL was not found'
+    sys.exit(1)
 
-for dl_i, dl_path in enumerate(dl_paths):
-    sys.stdout.write('Downloading file %d of %d, %s...' % (dl_i+1, len(dl_paths), dl_path))
-    sys.stdout.flush()
-    f = urllib2.urlopen('%s/%s' % (url, dl_path))
-    with open('%s/%s' % (target_dir, dl_path), 'wb') as local_file:
-        local_file.write(f.read())
-    print 'done.'
+  print 'found m3u at path %s' % m3u_path
+
+  # download m3u file
+  m3u_url = '%s%s' % (archive_root, m3u_path)
+  print 'downloading m3u from URL %s' % m3u_url
+  mp3_list = [line.strip() for line in requests.get(m3u_url).text.split('\n') if line.strip()]
+  print 'found %d files' % len(mp3_list)
+
+  # create output folder
+  output_dir = os.path.join(os.getenv('HOME'), 'Desktop', os.path.basename(show_url.path).split('.')[0])
+  print 'creating output directory %s' % output_dir
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+  # download each song file
+  for mp3_num, mp3_url in enumerate(mp3_list):
+    print 'downloading %d of %d' % (mp3_num+1, len(mp3_list))
+    print '--> src: %s' % mp3_url
+    mp3_dest = os.path.join(output_dir, os.path.basename(urlparse(mp3_url).path))
+    print '--> dest: %s' % mp3_dest
+    mp3_resp = requests.get(mp3_url, stream=True)
+    if mp3_resp.status_code == 200:
+      total_len = float(mp3_resp.headers.get('content-length'))
+      progress = 0
+      sys.stdout.write('--> progress: 0%')
+      with open(mp3_dest, 'wb') as fp:
+        for chunk in mp3_resp.iter_content(DL_CHUNK_SIZE):
+          fp.write(chunk)
+          progress += DL_CHUNK_SIZE
+          sys.stdout.write('\r--> progress: %d%%' % (100 * progress / total_len))
+      print ''
+    else:
+      print '--> error %d: %s' % (mp3_resp.status_code, mp3_resp.text)
+
+  print 'done!'
+
+if __name__ == '__main__':
+  main()
