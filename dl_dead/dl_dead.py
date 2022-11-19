5bcfe071f28a427f04d95204f2e9c89c3941f4af
Brian Cipriano <cipriano@google.com>
2022-11-19 12:13:04 -0500

    Update for Python 3.

--- dl_dead/dl_dead.py
+++ dl_dead/dl_dead.py
@@ -14,8 +14,7 @@ soundboard shows. If used it should only be used to download freely available sh
 import argparse
 import os
 import sys
-import urllib2
-from urlparse import urlparse
+import urllib
 
 import requests
 
@@ -27,11 +26,11 @@ def main():
   parser.add_argument('dest_dir', help='directory to download to')
   args = parser.parse_args()
 
-  print 'downloading show from %s' % args.show_url
+  print('downloading show from %s' % args.show_url)
 
-  show_url = urlparse(args.show_url)
+  show_url = urllib.parse.urlparse(args.show_url)
   archive_root = '%s://%s' % (show_url.scheme, show_url.netloc)
-  print 'archive root %s' % archive_root
+  print('archive root %s' % archive_root)
 
   # get page source
   details_src = requests.get(args.show_url).text
@@ -47,29 +46,29 @@ def main():
       break
 
   if m3u_path is None:
-    print 'm3u URL was not found'
+    print('m3u URL was not found')
     sys.exit(1)
 
-  print 'found m3u at path %s' % m3u_path
+  print('found m3u at path %s' % m3u_path)
 
   # download m3u file
   m3u_url = '%s%s' % (archive_root, m3u_path)
-  print 'downloading m3u from URL %s' % m3u_url
+  print('downloading m3u from URL %s' % m3u_url)
   mp3_list = [line.strip() for line in requests.get(m3u_url).text.split('\n') if line.strip()]
-  print 'found %d files' % len(mp3_list)
+  print('found %d files' % len(mp3_list))
 
   # create output folder
   output_dir = os.path.join(os.path.abspath(args.dest_dir), os.path.basename(show_url.path).split('.')[0])
-  print 'creating output directory %s' % output_dir
+  print('creating output directory %s' % output_dir)
   if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
   # download each song file
   for mp3_num, mp3_url in enumerate(mp3_list):
-    print 'downloading %d of %d' % (mp3_num+1, len(mp3_list))
-    print '--> src: %s' % mp3_url
-    mp3_dest = os.path.join(output_dir, os.path.basename(urlparse(mp3_url).path))
-    print '--> dest: %s' % mp3_dest
+    print('downloading %d of %d' % (mp3_num+1, len(mp3_list)))
+    print('--> src: %s' % mp3_url)
+    mp3_dest = os.path.join(output_dir, os.path.basename(urllib.parse.urlparse(mp3_url).path))
+    print('--> dest: %s' % mp3_dest)
     mp3_resp = requests.get(mp3_url, stream=True)
     if mp3_resp.status_code == 200:
       total_len = float(mp3_resp.headers.get('content-length'))
@@ -80,11 +79,11 @@ def main():
           fp.write(chunk)
           progress += DL_CHUNK_SIZE
           sys.stdout.write('\r--> progress: %d%%' % (100 * progress / total_len))
-      print ''
+      print('')
     else:
-      print '--> error %d: %s' % (mp3_resp.status_code, mp3_resp.text)
+      print('--> error %d: %s' % (mp3_resp.status_code, mp3_resp.text))
 
-  print 'done!'
+  print('done!')
 
 if __name__ == '__main__':
   main()
